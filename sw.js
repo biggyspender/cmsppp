a04c0a29c279acde7d110e988e25705bedd6b915
spender <christophermartinsperry@gmail.com>
2019-08-01 00:43:14 +0100

    magiic

--- sw.js
+++ sw.js
@@ -1,16 +1,19 @@
-self.addEventListener('install', function(event) {
-    // Perform install steps
-    event.waitUntil(
-      Promise.resolve()
-    );
-  });
-
-
+self.addEventListener("install", function(event) {
+  // Perform install steps
+  event.waitUntil(Promise.resolve());
+});
 
 self.addEventListener("fetch", function(event) {
   console.log("intercepted");
+  let req =
+    event.request.url === "https://biggyspender.github.io/cmsppp/trailer1.mp4"
+      ? {
+          ...event.request,
+          url: "https://biggyspender.github.io/cmsppp/trailer.mp4"
+        }
+      : event.request;
   event.respondWith(
-    fetch(event.request)
+    fetch(req)
     //   caches.match(event.request)
     //     .then(function(response) {
     //       // Cache hit - return response
