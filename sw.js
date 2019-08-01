36d20dfdd1b260655a1b2ec7660e61e886170907
spender <christophermartinsperry@gmail.com>
2019-08-01 01:43:02 +0100

    woowoo

--- sw.js
+++ sw.js
@@ -1,43 +1,38 @@
-var CACHE_NAME = "cmsppp_fpp";
-var urlsToCache = [];
-
-// Set the callback for the install step
 self.addEventListener("install", function(event) {
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
+    Promise.resolve().then(() => {
+      console.log("I am finished installing");
     })
   );
 });
 
 self.addEventListener("fetch", function(event) {
-  console.log("tom is soooooo coool");
+  console.log("intercepted");
+  //   if(event.request.url === "https://biggyspender.github.io/cmsppp/trailer1.mp4"){
+  //       event.request.url="https://biggyspender.github.io/cmsppp/trailer.mp4"
+  //   }
+  //   let req =
+  //     event.request.url === "https://biggyspender.github.io/cmsppp/trailer1.mp4"
+  //       ? {
+  //           ...event.request,
+  //           url: "https://biggyspender.github.io/cmsppp/trailer.mp4"
+  //         }
+  //       : event.request;
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-
-      return fetch(event.request);
-    })
+    fetch(event.request)
+    //   caches.match(event.request)
+    //     .then(function(response) {
+    //       // Cache hit - return response
+    //       if (response) {
+    //         return response;
+    //       }
+    //       return fetch(event.request);
+    //     }
+    //   )
   );
 });
 
 self.addEventListener("activate", function(event) {
-  var cacheWhitelist = [];
-
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+  event.waitUntil(self.clients.claim());
 });
