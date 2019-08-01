86f9215fdb935c2f78b974cfcbc85fe2ff6fd394
spender <christophermartinsperry@gmail.com>
2019-08-01 01:08:45 +0100

    efwfe

--- sw.js
+++ sw.js
@@ -1,20 +1,22 @@
 self.addEventListener("install", function(event) {
   // Perform install steps
-  event.waitUntil(Promise.resolve());
+  event.waitUntil(Promise.resolve()).then(() => {
+    console.log("I am finished installing");
+  });
 });
 
 self.addEventListener("fetch", function(event) {
   console.log("intercepted");
-//   if(event.request.url === "https://biggyspender.github.io/cmsppp/trailer1.mp4"){
-//       event.request.url="https://biggyspender.github.io/cmsppp/trailer.mp4"
-//   }
-//   let req =
-//     event.request.url === "https://biggyspender.github.io/cmsppp/trailer1.mp4"
-//       ? {
-//           ...event.request,
-//           url: "https://biggyspender.github.io/cmsppp/trailer.mp4"
-//         }
-//       : event.request;
+  //   if(event.request.url === "https://biggyspender.github.io/cmsppp/trailer1.mp4"){
+  //       event.request.url="https://biggyspender.github.io/cmsppp/trailer.mp4"
+  //   }
+  //   let req =
+  //     event.request.url === "https://biggyspender.github.io/cmsppp/trailer1.mp4"
+  //       ? {
+  //           ...event.request,
+  //           url: "https://biggyspender.github.io/cmsppp/trailer.mp4"
+  //         }
+  //       : event.request;
   event.respondWith(
     fetch(event.request)
     //   caches.match(event.request)
