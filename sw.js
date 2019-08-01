fd345944b689366869484e0641b663a7355e2a1c
spender <christophermartinsperry@gmail.com>
2019-08-01 01:48:07 +0100

    interceptor

--- sw.js
+++ sw.js
@@ -12,15 +12,15 @@ self.addEventListener("fetch", function(event) {
   //   if(event.request.url === "https://biggyspender.github.io/cmsppp/trailer1.mp4"){
   //       event.request.url="https://biggyspender.github.io/cmsppp/trailer.mp4"
   //   }
-  //   let req =
-  //     event.request.url === "https://biggyspender.github.io/cmsppp/trailer1.mp4"
-  //       ? {
-  //           ...event.request,
-  //           url: "https://biggyspender.github.io/cmsppp/trailer.mp4"
-  //         }
-  //       : event.request;
+    let req =
+      event.request.url === "https://biggyspender.github.io/cmsppp/trailer1.mp4"
+        ? {
+            ...event.request,
+            url: "https://biggyspender.github.io/cmsppp/trailer.mp4"
+          }
+        : event.request;
   event.respondWith(
-    fetch(event.request)
+    fetch(req)
     //   caches.match(event.request)
     //     .then(function(response) {
     //       // Cache hit - return response
