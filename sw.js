186cfb32e0987fbce8db3c03071c660f7863b734
spender <christophermartinsperry@gmail.com>
2019-08-01 01:02:45 +0100

    sim

--- sw.js
+++ sw.js
@@ -5,9 +5,9 @@ self.addEventListener("install", function(event) {
 
 self.addEventListener("fetch", function(event) {
   console.log("intercepted");
-  if(event.request.url === "https://biggyspender.github.io/cmsppp/trailer1.mp4"){
-      event.request.url="https://biggyspender.github.io/cmsppp/trailer.mp4"
-  }
+//   if(event.request.url === "https://biggyspender.github.io/cmsppp/trailer1.mp4"){
+//       event.request.url="https://biggyspender.github.io/cmsppp/trailer.mp4"
+//   }
 //   let req =
 //     event.request.url === "https://biggyspender.github.io/cmsppp/trailer1.mp4"
 //       ? {
