424673e7ddd6d883dd9a406687cd0e419e018921
spender <christophermartinsperry@gmail.com>
2019-08-01 01:13:11 +0100

    ewgw

--- sw.js
+++ sw.js
@@ -1,34 +1,29 @@
 self.addEventListener("install", function(event) {
   // Perform install steps
-  event.waitUntil(
-    Promise.resolve().then(() => {
-      console.log("I am finished installing");
-    })
-  );
-});
-
-self.addEventListener("fetch", function(event) {
-  console.log("intercepted");
-  //   if(event.request.url === "https://biggyspender.github.io/cmsppp/trailer1.mp4"){
-  //       event.request.url="https://biggyspender.github.io/cmsppp/trailer.mp4"
-  //   }
-  //   let req =
-  //     event.request.url === "https://biggyspender.github.io/cmsppp/trailer1.mp4"
-  //       ? {
-  //           ...event.request,
-  //           url: "https://biggyspender.github.io/cmsppp/trailer.mp4"
-  //         }
-  //       : event.request;
-  event.respondWith(
-    fetch(event.request)
-    //   caches.match(event.request)
-    //     .then(function(response) {
-    //       // Cache hit - return response
-    //       if (response) {
-    //         return response;
-    //       }
-    //       return fetch(event.request);
-    //     }
-    //   )
-  );
+  console.log("I am installing");
+  self.addEventListener("fetch", function(event) {
+    console.log("intercepted");
+    //   if(event.request.url === "https://biggyspender.github.io/cmsppp/trailer1.mp4"){
+    //       event.request.url="https://biggyspender.github.io/cmsppp/trailer.mp4"
+    //   }
+    //   let req =
+    //     event.request.url === "https://biggyspender.github.io/cmsppp/trailer1.mp4"
+    //       ? {
+    //           ...event.request,
+    //           url: "https://biggyspender.github.io/cmsppp/trailer.mp4"
+    //         }
+    //       : event.request;
+    event.respondWith(
+      fetch(event.request)
+      //   caches.match(event.request)
+      //     .then(function(response) {
+      //       // Cache hit - return response
+      //       if (response) {
+      //         return response;
+      //       }
+      //       return fetch(event.request);
+      //     }
+      //   )
+    );
+  });
 });
