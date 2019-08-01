5259fc9abc81a9e376d9b31811ed78bb1dc018f2
spender <christophermartinsperry@gmail.com>
2019-08-01 01:22:39 +0100

    tom is cool

--- sw.js
+++ sw.js
@@ -1,29 +1,43 @@
+var CACHE_NAME = "cmsppp_fpp";
+var urlsToCache = [];
+
+// Set the callback for the install step
 self.addEventListener("install", function(event) {
   // Perform install steps
-  console.log("I am installing");
-  self.addEventListener("fetch", function(event) {
-    console.log("intercepted");
-    //   if(event.request.url === "https://biggyspender.github.io/cmsppp/trailer1.mp4"){
-    //       event.request.url="https://biggyspender.github.io/cmsppp/trailer.mp4"
-    //   }
-    //   let req =
-    //     event.request.url === "https://biggyspender.github.io/cmsppp/trailer1.mp4"
-    //       ? {
-    //           ...event.request,
-    //           url: "https://biggyspender.github.io/cmsppp/trailer.mp4"
-    //         }
-    //       : event.request;
-    event.respondWith(
-      fetch(event.request)
-      //   caches.match(event.request)
-      //     .then(function(response) {
-      //       // Cache hit - return response
-      //       if (response) {
-      //         return response;
-      //       }
-      //       return fetch(event.request);
-      //     }
-      //   )
-    );
-  });
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  console.log("tom is soooooo coool");
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  var cacheWhitelist = [];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
